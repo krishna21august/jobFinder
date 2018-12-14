73276629f896874330a4efac35c50f76cac9a020
safe <krishna.s@lucideustech.com>
2018-12-14 09:16:02 +0530

    set veiew engine and seed data

--- routes/gigs.js
+++ routes/gigs.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require("../config/database");
 const Gig = require("../models/gig");
 
+/*Get list of gigs */
 router.get("/", (req, res) =>
   Gig.findAll()
     .then(gigs => {
@@ -14,5 +15,28 @@ router.get("/", (req, res) =>
       res.sendStatus(400);
     })
 );
+
+//add a new gig jsu to manually add the data this is not doen ideallly
+router.get("/add", (req, res) => {
+  const data = {
+    title: "Project Manager",
+    description: "3 year experience required",
+    technologies: "JIRA,Agile",
+    budget: "80000",
+    contact_email: "anand.s@lucideustech.com"
+  };
+
+  let { title, description, technologies, budget, contact_email } = data;
+
+  Gig.create({
+    title,
+    technologies,
+    budget,
+    contact_email,
+    description
+  })
+    .then(gig => res.redirect("/gigs"))
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
