89d75614b9e3a11d29862a5223f2dae1852cb580
safe <krishna.s@lucideustech.com>
2018-12-18 14:38:44 +0530

    landing page of code gig

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const path = require("path");
 const bodyParser = require("body-parser");
+const path = require("path");
+
+// Database
 const db = require("./config/database");
 
+// Test DB
 db.authenticate()
-  .then(() => console.log("db connected"))
-  .catch(err => console.log("Some error occured:" + err));
+  .then(() => console.log("Database connected..."))
+  .catch(err => console.log("Error: " + err));
 
 const app = express();
 
-//middleware for handelbars
-//we are setting handlebars as our template engine and setting default layout to main.js
+// Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-/*To set 'public' folder as statc folder*/
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.send("Hello Project started");
-});
+// Index route
+app.get("/", (req, res) => res.render("index", { layout: "landing" }));
 
-/*Routes  */
+// Gig routes
 app.use("/gigs", require("./routes/gigs"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`app has started at ${PORT}`));
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
