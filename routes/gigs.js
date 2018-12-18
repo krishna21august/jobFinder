358607ba1bacb784b763373421ba7b87a0bcb408
krishna21august <krishnakant.singh.min12@gmail.com>
2018-12-18 19:04:05 +0530

    searcha nd addition functionality

--- routes/gigs.js
+++ routes/gigs.js
@@ -2,13 +2,15 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/database");
 const Gig = require("../models/gig");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
-/*Get list of gigs */
+/*Get list of gigs.If key and value of object same, we can just go with keys */
 router.get("/", (req, res) =>
   Gig.findAll()
     .then(gigs => {
       console.log(gigs);
-      res.sendStatus(200);
+      res.render("gigs", { gigs });
     })
     .catch(err => {
       console.log(err);
@@ -16,26 +18,65 @@ router.get("/", (req, res) =>
     })
 );
 
-//add a new gig jsu to manually add the data this is not doen ideallly
+//view form page to add gigs
 router.get("/add", (req, res) => {
-  const data = {
-    title: "Project Manager",
-    description: "3 year experience required",
-    technologies: "JIRA,Agile",
-    budget: "80000",
-    contact_email: "anand.s@lucideustech.com"
-  };
+  res.render("add");
+});
 
-  let { title, description, technologies, budget, contact_email } = data;
+//add a new gig jsu to manually add the data this is not doen ideallly
+router.post("/add", (req, res) => {
+  let { title, description, technologies, budget, contact_email } = req.body;
+  let errors = [];
 
-  Gig.create({
-    title,
-    technologies,
-    budget,
-    contact_email,
-    description
-  })
-    .then(gig => res.redirect("/gigs"))
+  if (!title) {
+    errors.push({ text: "title is required" });
+  }
+
+  if (!description) {
+    errors.push({ text: "description is required" });
+  }
+
+  if (!technologies) {
+    errors.push({ text: "technology is required" });
+  }
+
+  if (!contact_email) {
+    errors.push({ text: "contact email is required" });
+  }
+
+  if (errors.length > 0) {
+    res.render("add", {
+      errors,
+      title,
+      technologies,
+      budget,
+      contact_email,
+      description
+    });
+  } else {
+    if (!budget) {
+      budget = "unknown";
+    } else {
+      budget = `$${budget}`;
+    }
+    technologies = technologies.toLowerCase().replace(/, /g, ",");
+
+    Gig.create({
+      title,
+      technologies,
+      budget,
+      contact_email,
+      description
+    })
+      .then(gig => res.redirect("/gigs"))
+      .catch(err => console.log(err));
+  }
+});
+
+router.get("/search", (req, res) => {
+  const { term } = req.query;
+  Gig.findAll({ where: { technologies: { [Op.like]: "%" + term + "%" } } })
+    .then(gigs => res.render("gigs", { gigs }))
     .catch(err => console.log(err));
 });
 
